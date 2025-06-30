04478b481f28fd57d571665e2ce28363c1dce057
ToanLK <khanhtoan.le.6555@gmail.com>
2025-06-30 17:32:59 +0700

    first commit

---
+++ MyCSharpProject/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCSharpProject.Controllers;
+
+public class UserController : ControllerBase
+{
+    [HttpGet("")]
+    public List<string> GetUser()
+    {
+        return new List<string>()
+        {
+            "Toan", "Khanh", "Le"
+        };
+    }
+}
